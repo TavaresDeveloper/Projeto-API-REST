0e8d201dee5e89b0db193aad7994721c4774ea35
TavaresDeveloper <Tavares_23456@outlook.com>
2025-08-24 23:28:19 -0300

    Atualizações de segurança

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const mysql2 = require('mysql2/promise');
 
+require('dotenv').config();
 
 
 const api = express();
@@ -10,10 +11,10 @@ const port = 3000;
 
     const dbConfig = {
       
-        host: 'localhost',
-        user: 'root',
-        password: 'Moder23@@',
-        database: 'empresa',
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
 
     };
     
